24e3e37c5cfdd95475b73555f105e161b8c7e4a2
thedev-junyoung <thedev.junyoung@gmail.com>
2024-09-12 22:29:30 +0900

    ENUM 명 변경

--- src/main/java/com/example/runshop/model/enums/OrderStatus.java
+++ src/main/java/com/example/runshop/model/enums/OrderStatus.java
@@ -1,9 +1,9 @@
 package com.example.runshop.model.enums;
 
 public enum OrderStatus {
-    PAYMENT_WAITING,
-    PAYMENT_COMPLETE,
-    SHIPPING_IN_PROGRESS,
-    DELIVERY_COMPLETE,
-    ORDER_CANCELLATION
+    PENDING, // 주문 대기
+    PAYMENT_COMPLETE, // 결제 완료
+    SHIPPING_IN_PROGRESS, // 배송 중
+    DELIVERY_COMPLETE, // 배송 완료
+    ORDER_CANCELLATION // 주문 취소
 }
